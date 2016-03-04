81972ac3a6921539858a92f352cc0eb15e972e3b
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-03-04 10:58:44 -0600

    assigning value to boxes per turn

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,28 @@
 (function () {
   'use strict';
 
-  const squareName = document.querySelectorAll('input[name]');
+  //assigning the inputs into a variable to use.
+  const squareName = document.querySelectorAll('input');
 
-  console.log('squareName', squareName);
+  let count = 0;
 
+  //looping through all my inputs to find name values upon click.
   for (var i = 0; i < squareName.length; i++) {
+
     squareName[i].addEventListener('click', function (event) {
-      console.log('button works', event.target.name);
+
+        //counting each click as a turn
+        count++;
+
+        //alternating a value of x or o to each turn based on remainder
+        if (count % 2 !== 0){
+          event.target.value = 'x';
+        } else {
+          event.target.value = 'o';
+        }
+        console.log('value', event.target.value);
     });
+
 
    }
 
